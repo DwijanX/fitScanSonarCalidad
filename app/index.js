937f29a02da82f0b27aa7ca877cbae79a1c3637d
Christian Rivero <christianriveroarnez@gmail.com>
2023-04-28 20:03:24 -0400

    added fonts

--- app/index.js
+++ app/index.js
@@ -1,28 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import '/css/styles.css'
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'LexendExtraBold': require('./assets/fonts/static/Lexend-ExtraBold.ttf'),
+    'LexendBold': require('./assets/fonts/static/Lexend-Bold.ttf'),
+    'LexendNormal': require('./assets/fonts/static/Lexend-Medium.ttf'),
+    'LexendLight': require('./assets/fonts/static/Lexend-Light.ttf'),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null
+  }
   return (
-    <View style="container">
-      <View style="header">
-        <View style="titleContainer">
-          <Text style="title">FitScan</Text>
-          <View style="circle" />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>FitScan</Text>
+          <View style={styles.circle} />
         </View>
       </View>
-      <View style={["box", "box1"]}>
-        <Text style="boxText">Reporte Diario</Text>
+      <View style={[styles.box, styles.box1]}>
+        <Text style={styles.boxText}>Reporte Diario</Text>
       </View>
-      <View style="row">
-        <View style={["box", "box2a"]}>
-          <Text style="boxText">Foto Scan</Text>
+      <View style={styles.row}>
+        <View style={[styles.box, styles.box2a]}>
+          <Text style={styles.boxText}>Foto Scan</Text>
         </View>
-        <View style={["box", "box2b"]}>
-          <Text style="boxText">Input Manual</Text>
+        <View style={[styles.box, styles.box2b]}>
+          <Text style={styles.boxText}>Input Manual</Text>
         </View>
       </View>
-      <View style={["box", "box3"]}>
-        <Text style="boxText">Mi Manual</Text>
+      <View style={[styles.box, styles.box3]}>
+        <Text style={styles.boxText}>Box 3</Text>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -30,4 +49,74 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2f4858',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    height: 80,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontFamily: 'LexendExtraBold',
+    fontSize: 20,
+    marginTop: 30,
+    color: '#FFF',
+  },
+  circle: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginLeft: 5,
+  },
+  box: {
+    height: 220,
+    width: 150,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    flexDirection: 'column-reverse',
+  },
+  box1: {
+    height: 160,
+    width: 300,
+    backgroundColor: '#9EE493',
+    marginVertical: 20,
+  },
+  box2a: {
+    backgroundColor: '#336699',
+    marginRight: 5,
+  },
+  box2b: {
+    backgroundColor: '#DAF7DC',
+    marginLeft: 5,
+  },
+  box3: {
+    width: 300,
+    height: 150,
+    backgroundColor: '#86BBD8',
+    marginTop: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
+  boxText: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: '#000',
+    fontFamily: "LexendBold",
+  },
 });
