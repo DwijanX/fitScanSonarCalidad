ece0743994a559e5d059a5b853019903b0c2dca5
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-24 15:42:47 -0400

    removed remaining code smells

--- app/inputManual.js
+++ app/inputManual.js
@@ -1,44 +1,43 @@
-import React, { useState } from 'react';
-import { Pressable , StyleSheet, Text, View, TextInput } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons'
-import baseStyles from './styles/baseStyles'
-import { useRouter } from 'expo-router';
-import { useFonts } from 'expo-font';
-import * as database from './dataBase/databaseCalls'
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View, TextInput } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import baseStyles from "./styles/baseStyles";
+import { useRouter } from "expo-router";
+import { useFonts } from "expo-font";
+import * as database from "./dataBase/databaseCalls";
 
 function InputManual() {
-  const router=useRouter()
+  const router = useRouter();
   useFonts({
-    'LexendExtraBold': require('./assets/fonts/static/Lexend-ExtraBold.ttf'),
-    'LexendBold': require('./assets/fonts/static/Lexend-Bold.ttf'),
-    'LexendNormal': require('./assets/fonts/static/Lexend-Medium.ttf'),
-    'LexendLight': require('./assets/fonts/static/Lexend-Light.ttf'),
+    LexendExtraBold: require("./assets/fonts/static/Lexend-ExtraBold.ttf"),
+    LexendBold: require("./assets/fonts/static/Lexend-Bold.ttf"),
+    LexendNormal: require("./assets/fonts/static/Lexend-Medium.ttf"),
+    LexendLight: require("./assets/fonts/static/Lexend-Light.ttf"),
   });
 
-  const [nombre, setNombre] = useState('');
-  const [calorias, setCalorias] = useState('');
-  const [ingredientes, setIngredientes] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [calorias, setCalorias] = useState("");
 
   let now = new Date();
   now.setHours(now.getHours() - 4);
   let date = now.toISOString().substring(0, 10).replace(/-/g, "_");
 
-
   const sendFood = () => {
     //console.log("juan",[nombre],[parseInt(calorias)],date)
-    database.newDishesConsumed("juan",[nombre],[parseInt(calorias)],date)
-    router.back()
+    database.newDishesConsumed("juan", [nombre], [parseInt(calorias)], date);
+    router.back();
   };
 
   return (
     <View style={styles.container}>
-        <View style={styles.headerContainer}>
-          <Pressable style={styles.backButton} onPress={()=>router.back()}>
-              
-              <Ionicons name="arrow-back" size={32}></Ionicons>
-          </Pressable>
-          <Text style={[styles.Title3,styles.pageTitle]} title="test">Input Manual</Text>
-        </View>
+      <View style={styles.headerContainer}>
+        <Pressable style={styles.backButton} onPress={() => router.back()}>
+          <Ionicons name="arrow-back" size={32}></Ionicons>
+        </Pressable>
+        <Text style={[styles.Title3, styles.pageTitle]} title="test">
+          Input Manual
+        </Text>
+      </View>
       <View style={styles.box}>
         <Text style={styles.label}>Nombre</Text>
         <TextInput
@@ -59,50 +58,51 @@ function InputManual() {
         />
       </View>
       <Pressable style={styles.addButton} onPress={sendFood}>
-          <Text style={styles.buttonText}>Añadir</Text>
+        <Text style={styles.buttonText}>Añadir</Text>
       </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({...baseStyles,...{
-  container: {
-    flex: 1,
-    backgroundColor: '#2f4858',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create({
+  ...baseStyles,
+  ...{
+    container: {
+      flex: 1,
+      backgroundColor: "#2f4858",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    box: {
+      backgroundColor: "#336699",
+      borderRadius: 10,
+      padding: 10,
+      marginVertical: 10,
+      marginBottom: 20,
+      width: "80%",
+      height: 120,
+    },
+    label: {
+      marginBottom: 5,
+      fontFamily: "LexendBold",
+      fontSize: 22,
+    },
+    smallLabel: {
+      marginBottom: 5,
+      fontFamily: "LexendLight",
+      fontSize: 14,
+    },
+    input: {
+      backgroundColor: "#FFF",
+      padding: 10,
+      fontSize: 16,
+      fontFamily: "LexendNormal",
+    },
+    multilineInput: {
+      height: 100,
+      textAlignVertical: "top",
+    },
   },
-  box: {
-    backgroundColor: '#336699',
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 10,
-    marginBottom: 20,
-    width: '80%',
-    height:120,
-    
-  },
-  label: {
-    marginBottom: 5,
-    fontFamily: 'LexendBold',
-    fontSize: 22,
-  },
-  smallLabel: {
-    marginBottom: 5,
-    fontFamily: 'LexendLight',
-    fontSize: 14,
-  },
-  input: {
-    backgroundColor: '#FFF',
-    padding: 10,
-    fontSize: 16,
-    fontFamily: 'LexendNormal',
-  },
-  multilineInput: {
-    height: 100,
-    textAlignVertical: 'top',
-  },
-}
 });
 
 export default InputManual;
--- app/reporte.js
+++ app/reporte.js
@@ -1,91 +1,115 @@
-import React, { useState,useEffect } from 'react';
-import { Pressable , StyleSheet, Text, View } from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons'
-import baseStyles from './styles/baseStyles'
-import { useRouter } from 'expo-router';
-import { useFonts } from 'expo-font';
-import * as database from './dataBase/databaseCalls'
-
+import React, { useState, useEffect } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import baseStyles from "./styles/baseStyles";
+import { useRouter } from "expo-router";
+import { useFonts } from "expo-font";
+import * as database from "./dataBase/databaseCalls";
 
 function Reporte() {
+  const router = useRouter();
+  const [alimentos, setAlimentos] = useState([
+    { nombre: "zapallo", calorias: 100 },
+    { nombre: "cebolla", calorias: 200 },
+  ]);
+  const [totalCalories, setCalories] = useState(0);
+  const [, setCaloriesUser] = useState(0);
+  const [cumplisteDieta, setDietaMeta] = useState(0);
+  let now = new Date();
+  now.setHours(now.getHours() - 4);
+  let fecha = now.toISOString().substring(0, 10);
 
-  const router=useRouter()
-  const [alimentos,setAlimentos] = useState([{ nombre: 'zapallo', calorias: 100 }, { nombre: 'cebolla', calorias: 200 }])
-  const [totalCalories, setCalories] = useState(0)
-  const [caloriasUsuario, setCaloriesUser] = useState(0)
-  const [cumplisteDieta, setDietaMeta] = useState(0)
-    let now = new Date();
-    now.setHours(now.getHours() - 4);
-    let fecha = now.toISOString().substring(0, 10);
-
-  useEffect(()=>{
+  useEffect(() => {
     let now = new Date();
     now.setHours(now.getHours() - 4);
     let fecha = now.toISOString().substring(0, 10).replace(/-/g, "_");
-    const getFood = async()=>{
-        let alimentosDatabase = await database.getFoodOfADate("juan",fecha)
-        let userCalories = await database.getUserCalories("juan")
-        console.log(userCalories)
-        let totalCalories = 0
-        let alimentosKeys = Object.keys(alimentosDatabase)
-        for (const alimentosKey of alimentosKeys) {
-          totalCalories += alimentosDatabase[alimentosKey]
-          
-        }
+    const getFood = async () => {
+      let alimentosDatabase = await database.getFoodOfADate("juan", fecha);
+      let userCalories = await database.getUserCalories("juan");
+      console.log(userCalories);
+      let totalCalories = 0;
+      let alimentosKeys = Object.keys(alimentosDatabase);
+      for (const alimentosKey of alimentosKeys) {
+        totalCalories += alimentosDatabase[alimentosKey];
+      }
 
-        console.log(totalCalories)
-        console.log(userCalories)
-        console.log(userCalories-totalCalories)
+      console.log(totalCalories);
+      console.log(userCalories);
+      console.log(userCalories - totalCalories);
 
-        setAlimentos(alimentosDatabase)
-        setCalories(totalCalories)
-        setCaloriesUser(userCalories)
-        setDietaMeta(userCalories-totalCalories)
-    }
-    getFood()
-    },[])
-  
+      setAlimentos(alimentosDatabase);
+      setCalories(totalCalories);
+      setCaloriesUser(userCalories);
+      setDietaMeta(userCalories - totalCalories);
+    };
+    getFood();
+  }, []);
+
   useFonts({
-    'LexendExtraBold': require('./assets/fonts/static/Lexend-ExtraBold.ttf'),
-    'LexendBold': require('./assets/fonts/static/Lexend-Bold.ttf'),
-    'LexendNormal': require('./assets/fonts/static/Lexend-Medium.ttf'),
-    'LexendLight': require('./assets/fonts/static/Lexend-Light.ttf'),
+    LexendExtraBold: require("./assets/fonts/static/Lexend-ExtraBold.ttf"),
+    LexendBold: require("./assets/fonts/static/Lexend-Bold.ttf"),
+    LexendNormal: require("./assets/fonts/static/Lexend-Medium.ttf"),
+    LexendLight: require("./assets/fonts/static/Lexend-Light.ttf"),
   });
 
   return (
     <View style={styles.mainContainer}>
-
       <View style={styles.headerContainer}>
-          <Pressable style={styles.backButton} onPress={()=>router.back()}>
-              
-              <Ionicons name="arrow-back" size={32}></Ionicons>
-          </Pressable>
-          <Text style={[styles.Title3,styles.pageTitle]} title="test">{fecha}</Text>
-        </View>
+        <Pressable style={styles.backButton} onPress={() => router.back()}>
+          <Ionicons name="arrow-back" size={32}></Ionicons>
+        </Pressable>
+        <Text style={[styles.Title3, styles.pageTitle]} title="test">
+          {fecha}
+        </Text>
+      </View>
 
-      <View style={[styles.blueBox, styles.box, styles.thirdBox, { flexDirection: 'row', justifyContent: 'space-between' }]}>
+      <View
+        style={[
+          styles.blueBox,
+          styles.box,
+          styles.thirdBox,
+          { flexDirection: "row", justifyContent: "space-between" },
+        ]}
+      >
         <Text style={styles.foodText}>Alimentos:</Text>
         <View style={[styles.ingredientesBox]}>
-            {Object.entries(alimentos).length ? (
-                <Text style={styles.foodText}>
-                {Object.entries(alimentos)
-                    .map(([nombre, calorias]) => `${nombre}: ${calorias}`)
-                    .join('\n')}
-                </Text>
-            ) : (
-                <Text style={styles.foodText}>no hay datos hoy</Text>
-            )}
+          {Object.entries(alimentos).length ? (
+            <Text style={styles.foodText}>
+              {Object.entries(alimentos)
+                .map(([nombre, calorias]) => `${nombre}: ${calorias}`)
+                .join("\n")}
+            </Text>
+          ) : (
+            <Text style={styles.foodText}>no hay datos hoy</Text>
+          )}
         </View>
       </View>
       <View style={[styles.lightBlueBox, styles.box, { marginTop: 10 }]}>
         <Text style={styles.foodText}>calorias totales: {totalCalories}</Text>
       </View>
-      {totalCalories != 0? (
-        <View style={[styles.HicisteBox, styles.box, { marginTop: 10, backgroundColor: cumplisteDieta>0 ? '#9EE493' : 'red' }]}>
-          <Text style={styles.foodText}>{cumplisteDieta>0 ?  'cumpliste Dieta' : 'no Cumpliste Dieta'}</Text>
+      {totalCalories != 0 ? (
+        <View
+          style={[
+            styles.HicisteBox,
+            styles.box,
+            {
+              marginTop: 10,
+              backgroundColor: cumplisteDieta > 0 ? "#9EE493" : "red",
+            },
+          ]}
+        >
+          <Text style={styles.foodText}>
+            {cumplisteDieta > 0 ? "cumpliste Dieta" : "no Cumpliste Dieta"}
+          </Text>
         </View>
-        ) : (
-        <View style={[styles.HicisteBox, styles.box, { marginTop: 10, backgroundColor:"#336699" }]}>
+      ) : (
+        <View
+          style={[
+            styles.HicisteBox,
+            styles.box,
+            { marginTop: 10, backgroundColor: "#336699" },
+          ]}
+        >
           <Text style={styles.foodText}>no hay datos hoy</Text>
         </View>
       )}
@@ -93,55 +117,57 @@ function Reporte() {
   );
 }
 
-const styles = StyleSheet.create({...baseStyles,...{
+const styles = StyleSheet.create({
+  ...baseStyles,
+  ...{
     header: {
-        height: 80,
-        width: '100%',
-        justifyContent: 'center',
-        flexDirection: 'row',
-      },
-      blueBox: {
-        backgroundColor: '#336699',
-        borderRadius: 10,
-        padding: 10,
+      height: 80,
+      width: "100%",
+      justifyContent: "center",
+      flexDirection: "row",
+    },
+    blueBox: {
+      backgroundColor: "#336699",
+      borderRadius: 10,
+      padding: 10,
     },
     lightBlueBox: {
-        backgroundColor: '#86BBD8',
-        borderRadius: 10,
-        padding: 10,
-        marginLeft: 10,
-        width: '75%'
-      },
-    greenBox: {
-        backgroundColor: '#86BBD8',
-        borderRadius: 10,
-        padding: 10,
-        marginLeft: 10,
-        width: '75%'
-      },
-    HicisteBox: {
-        borderRadius: 10,
-        padding: 10,
-        marginLeft: 10,
-        width: '60%',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      ingredientesBox: {
-        backgroundColor: '#86BBD8',
-        borderRadius: 10,
-        padding: 10,
-        marginLeft: 10,
-        width: '65%',
-        height: 200,
-        flexWrap: 'wrap', 
-      },
-    foodText: {
-        color: '#000',
-        fontSize: 16,
-        fontFamily: 'LexendBold',
+      backgroundColor: "#86BBD8",
+      borderRadius: 10,
+      padding: 10,
+      marginLeft: 10,
+      width: "75%",
     },
-}
+    greenBox: {
+      backgroundColor: "#86BBD8",
+      borderRadius: 10,
+      padding: 10,
+      marginLeft: 10,
+      width: "75%",
+    },
+    HicisteBox: {
+      borderRadius: 10,
+      padding: 10,
+      marginLeft: 10,
+      width: "60%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    ingredientesBox: {
+      backgroundColor: "#86BBD8",
+      borderRadius: 10,
+      padding: 10,
+      marginLeft: 10,
+      width: "65%",
+      height: 200,
+      flexWrap: "wrap",
+    },
+    foodText: {
+      color: "#000",
+      fontSize: 16,
+      fontFamily: "LexendBold",
+    },
+  },
 });
 
 export default Reporte;
