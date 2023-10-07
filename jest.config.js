ead42803ef1d9135b55978f628f6ba63e4803bfd
Christian <christianriveroarnez@gmail.com>
2023-10-07 13:35:04 -0400

    installed jest and testing library

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const config = {
+    "collectCoverageFrom": [
+        "app/**/*.{js,jsx,ts,tsx}",
+        "!<rootDir>/node_modules/"
+      ],
+    verbose: true,
+  };
+  
+module.exports = config;
