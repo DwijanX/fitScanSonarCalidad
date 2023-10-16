94c22f53365d3d29b152222b3923eff9d1a377ea
Gabriel Ichiro <gabriel582084@gmail.com>
2023-10-16 17:44:05 -0400

    removed duplicate key preset from jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 const config = {
   collectCoverageFrom: ["app/**/*.{js,jsx,ts,tsx}", "!<rootDir>/node_modules/"],
   verbose: true,
-  preset: "react-native",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   preset: "jest-expo",
   moduleNameMapper: {
